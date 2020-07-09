6c7e24258a90b501e9ef775715dbe208531efbed
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-07-09 08:31:23 +0800

    Update PSG1208.sh

--- padavan/PSG1208/PSG1208.sh
+++ padavan/PSG1208/PSG1208.sh
@@ -3,4 +3,3 @@ echo "CONFIG_FIRMWARE_CPU_600MHZ=y" >> .config
 
 # 添加插件
 echo "CONFIG_FIRMWARE_INCLUDE_SHADOWSOCKS=y" >> .config  # SS plus+
-echo "CONFIG_FIRMWARE_INCLUDE_ADBYBY=y" >> .config       # adbyby plus+
