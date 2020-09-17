334ad30472b3d300f99947b5fd8179364eee216b
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-09-17 19:46:27 +0800

    Delete PSG1218.sh

--- padavan/PSG1218/PSG1218.sh
+++
@@ -1,6 +0,0 @@
-# 是否超频 
-echo "CONFIG_FIRMWARE_CPU_600MHZ=y" >> .config
-
-# 添加插件
-echo "CONFIG_FIRMWARE_INCLUDE_SHADOWSOCKS=y" >> .config  # SS plus+
-echo "CONFIG_FIRMWARE_INCLUDE_ADBYBY=y" >> .config       # adbyby plus+
