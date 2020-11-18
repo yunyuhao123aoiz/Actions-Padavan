ab092270e3a5f7779b65ab381b1a3e164271c077
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-11-18 22:00:57 +0800

    Update PSG1208.sh

--- padavan/PSG1208/PSG1208.sh
+++ padavan/PSG1208/PSG1208.sh
@@ -2,11 +2,11 @@ DESTDIR=/opt/rt-n56u
 default_theme_path="${DESTDIR}/trunk/user/www/n56u_ribbon_fixed"
 
  echo "--------------开始复制默认配置文件----------"
-\cp -f ../public/defaults.h ${DESTDIR}/trunk/user/shared/defaults.h
+\cp -f ../PSG1208/defaults.h ${DESTDIR}/trunk/user/shared/defaults.h
  echo "--------------开始复制默认配置文件结束----------"
 
  echo "--------------开始复制主题----------------------"
 sudo rm -rf $default_theme_path/bootstrap
 sudo rm -rf $default_theme_path/images
-cp -rf ../public/theme/. $default_theme_path
+cp -rf ../PSG1208/theme/. $default_theme_path
  echo "--------------复制主题文件结束------------------"
