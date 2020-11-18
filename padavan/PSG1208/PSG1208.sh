53124594d7d8c031d5bbc804a7d97f1135607d61
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-11-19 07:35:08 +0800

    Update PSG1208.sh

--- padavan/PSG1208/PSG1208.sh
+++ padavan/PSG1208/PSG1208.sh
@@ -1,5 +1,6 @@
 DESTDIR=/opt/rt-n56u
 default_theme_path="${DESTDIR}/trunk/user/www/n56u_ribbon_fixed"
+shadowsocks_sh_path="${DESTDIR}/trunk/user/shadowsocks/scripts/shadowsocks.sh"
 
  echo "--------------开始复制默认配置文件----------"
 \cp -f ../PSG1208/defaults.h ${DESTDIR}/trunk/user/shared/defaults.h
@@ -10,3 +11,7 @@ sudo rm -rf $default_theme_path/bootstrap
 sudo rm -rf $default_theme_path/images
 cp -rf ../PSG1208/theme/. $default_theme_path
  echo "--------------复制主题文件结束------------------"
+
+ echo "--------------开始复制shadowsocks.sh----------------------"
+\cp -f ../PSG1208/shadowsocks.sh ${DESTDIR}/trunk/user/shadowsocks/scripts/shadowsocks.sh
+ echo "--------------复制shadowsocks.sh文件结束------------------"
