ad9bf6c626d57ee4df26f6d12039ed5bba31e485
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-11-18 21:40:05 +0800

    Update PSG1208.sh

--- padavan/PSG1208/PSG1208.sh
+++ padavan/PSG1208/PSG1208.sh
@@ -1,9 +1,12 @@
-# 是否超频 
-# echo "CONFIG_FIRMWARE_CPU_600MHZ=y" >> .config
-default_path='./user/shared/defaults.h'  # 默认文件配置目录
-echo "执行如下命令sed -i \"s/BOARD_PID/\"PHICOMM\"/g\" $default_path"
-sed -i "s/BOARD_PID/\"PHICOMM\"/g" $default_path
-default_theme_path='./user/www/n56u_ribbon_fixed'
+DESTDIR=/opt/rt-n56u
+default_theme_path="${DESTDIR}/trunk/user/www/n56u_ribbon_fixed"
+
+ echo "--------------开始复制默认配置文件----------"
+\cp -f ../public/defaults.h ${DESTDIR}/trunk/user/shared/defaults.h
+ echo "--------------开始复制默认配置文件结束----------"
+
+ echo "--------------开始复制主题----------------------"
 sudo rm -rf $default_theme_path/bootstrap
 sudo rm -rf $default_theme_path/images
-cp -rf ../theme/. $default_theme_path
+cp -rf ../public/theme/. $default_theme_path
+ echo "--------------复制主题文件结束------------------"
