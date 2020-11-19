693f3e7975999cd242e162d25e4c0ff6a690af57
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-11-19 08:02:30 +0800

    Update PSG1208.sh

--- padavan/PSG1208/PSG1208.sh
+++ padavan/PSG1208/PSG1208.sh
@@ -2,16 +2,16 @@ DESTDIR=/opt/rt-n56u
 default_theme_path="${DESTDIR}/trunk/user/www/n56u_ribbon_fixed"
 shadowsocks_sh_path="${DESTDIR}/trunk/user/shadowsocks/scripts"
 
- echo "--------------开始复制默认配置文件----------"
-\cp -f padavan/PSG1208/defaults.h ${DESTDIR}/trunk/user/shared/defaults.h
- echo "--------------开始复制默认配置文件结束----------"
+# echo "--------------开始复制默认配置文件----------"
+#\cp -f padavan/PSG1208/defaults.h ${DESTDIR}/trunk/user/shared/defaults.h
+# echo "--------------开始复制默认配置文件结束----------"
 
- echo "--------------开始复制主题----------------------"
-sudo rm -rf $default_theme_path/bootstrap
-sudo rm -rf $default_theme_path/images
-cp -rf padavan/PSG1208/theme/. $default_theme_path
- echo "--------------复制主题文件结束------------------"
+# echo "--------------开始复制主题----------------------"
+#sudo rm -rf $default_theme_path/bootstrap
+#sudo rm -rf $default_theme_path/images
+#cp -rf padavan/PSG1208/theme/. $default_theme_path
+# echo "--------------复制主题文件结束------------------"
 
- echo "--------------开始复制shadowsocks.sh----------------------"
-\cp -f padavan/PSG1208/shadowsocks.sh $shadowsocks_sh_path/shadowsocks.sh
- echo "--------------复制shadowsocks.sh文件结束------------------"
+# echo "--------------开始复制shadowsocks.sh----------------------"
+#\cp -f padavan/PSG1208/shadowsocks.sh $shadowsocks_sh_path/shadowsocks.sh
+# echo "--------------复制shadowsocks.sh文件结束------------------"
