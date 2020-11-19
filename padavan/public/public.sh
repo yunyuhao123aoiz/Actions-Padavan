d0133b6a98acec00295e62a94ace1c1428a6f3c9
Joelyi <37099326+Joelyi@users.noreply.github.com>
2020-11-19 08:02:03 +0800

    Update public.sh

--- padavan/public/public.sh
+++ padavan/public/public.sh
@@ -1,12 +1,17 @@
-# cd $WORK_PATH/trunk 执行在这个目录下
+DESTDIR=/opt/rt-n56u
+default_theme_path="${DESTDIR}/trunk/user/www/n56u_ribbon_fixed"
+shadowsocks_sh_path="${DESTDIR}/trunk/user/shadowsocks/scripts"
 
-# 1.修改一些默认参数 ./user/shared/defaults.h（需要不同设备不同拷贝到相应sh文件就好)
+ echo "--------------开始复制默认配置文件----------"
+\cp -f ../public/defaults.h ${DESTDIR}/trunk/user/shared/defaults.h
+ echo "--------------开始复制默认配置文件结束----------"
 
-user_name='admin'                        # 用户名 admin
-user_password=$user_name                 # 用户密码 admin
-lan_ip='192.168.2'                       # lan 地址 192.168.3.1 一定别写后面的 .1
-wifi_password='1234567890'               # wifi密码，切记密码最少8位 admin
-version_time=$(date "+%Y%m%d")           # 自动时间更新时版本号: 20200320
-default_path='./user/shared/defaults.h'  # 默认文件配置目录
+ echo "--------------开始复制主题----------------------"
+sudo rm -rf $default_theme_path/bootstrap
+sudo rm -rf $default_theme_path/images
+cp -rf ../public/theme/. $default_theme_path
+ echo "--------------复制主题文件结束------------------"
 
- 
+ echo "--------------开始复制shadowsocks.sh----------------------"
+\cp -f ../public/shadowsocks.sh $shadowsocks_sh_path/shadowsocks.sh
+ echo "--------------复制shadowsocks.sh文件结束------------------"
